60c532ab998f45fd9f18fbb1e2204935409121f9
chehsunliu <chehsunliu@gmail.com>
2023-03-21 00:12:32 +0800

    q

--- _zone/z-rust/src/main.rs
+++ _zone/z-rust/src/main.rs
@@ -1,35 +1,49 @@
 use std::io;
-use std::io::{ErrorKind, Write};
+use std::io::Write;
 
-fn solve(mut n: i64) -> Vec<i64> {
-    let mut ans = vec![];
-
-    while n != 1 {
-        ans.push(n);
-        n = if n % 2 == 0 { n / 2 } else { n * 3 + 1 }
+fn solve(a: i32, b: i32, c: i32, d: i32) -> i32 {
+    if d < b {
+        return -1;
     }
-    ans.push(1);
 
-    ans
+    if c > a + (d - b) {
+        return -1;
+    }
+
+    (d - b) + (a + (d - b) - c)
 }
 
 fn main() -> io::Result<()> {
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer)?;
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf)?;
+    buf.clear();
 
-    let n = buffer
-        .trim_end()
-        .parse()
-        .map_err(|e| io::Error::new(ErrorKind::Other, e))?;
-
-    let s = solve(n)
-        .into_iter()
-        .map(|v| v.to_string())
-        .collect::<Vec<String>>()
-        .join(" ")
-        + "\n";
-
-    io::stdout().write_all(s.as_bytes())?;
+    while io::stdin().read_line(&mut buf)? != 0 {
+        let mut iter = buf.trim_end().split_whitespace();
+        let ans = solve(
+            iter.next().unwrap().parse().unwrap(),
+            iter.next().unwrap().parse().unwrap(),
+            iter.next().unwrap().parse().unwrap(),
+            iter.next().unwrap().parse().unwrap(),
+        );
+        io::stdout().write_all((ans.to_string() + "\n").as_bytes())?;
+        buf.clear();
+    }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::solve;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(solve(-1, 0, -1, 2), 4);
+        assert_eq!(solve(0, 0, 4, 5), 6);
+        assert_eq!(solve(-2, -1, 1, 1), -1);
+        assert_eq!(solve(-3, 2, -3, 2), 0);
+        assert_eq!(solve(2, -1, -1, -1), 3);
+        assert_eq!(solve(1, 1, 0, 2), 3);
+    }
 }
