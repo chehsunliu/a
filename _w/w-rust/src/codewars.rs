442ea78442eb5128c7ba80b16fdadbfd811ed8d7
Che-Hsun Liu <chehsunliu@gmail.com>
2024-06-15 01:11:51 +0800

    [LeetCode] 167. Two Sum II - Input Array Is Sorted

--- _w/w-rust/src/codewars.rs
+++ _w/w-rust/src/codewars.rs
@@ -2,19 +2,19 @@ fn sort_array(arr: &[i32]) -> Vec<i32> {
     let mut odds_indexes = vec![];
     let mut odds = vec![];
     let mut ans = arr.iter().map(|v| *v).collect::<Vec<i32>>();
-    
+
     for (i, v) in arr.iter().enumerate() {
         if *v % 2 == 1 {
             odds_indexes.push(i);
             odds.push(*v);
         }
     }
-    
+
     odds.sort();
     for i in 0..odds.len() {
         ans[odds_indexes[i]] = odds[i];
     }
-    
+
     ans
 }
 
