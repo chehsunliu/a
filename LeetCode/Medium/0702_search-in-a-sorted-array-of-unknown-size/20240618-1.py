120205e04cc05acec25296a0373450654ef8ae76
Che-Hsun Liu <chehsunliu@gmail.com>
2024-06-18 02:13:18 +0800

    [LeetCode] 702. Search in a Sorted Array of Unknown Size

--- LeetCode/Medium/0702_search-in-a-sorted-array-of-unknown-size/20240618-1.py
+++ LeetCode/Medium/0702_search-in-a-sorted-array-of-unknown-size/20240618-1.py
@@ -6,28 +6,17 @@
 #    def get(self, index: int) -> int:
 
 
-def get_boundary(reader: 'ArrayReader') -> int:
-    l, r = 0, 10000 - 1
-
-    while l <= r:
-        m = (l + r) // 2
-        if reader.get(m) == 2147483647:
-            r = m - 1
-        else:
-            l = m + 1
-
-    return l
-
-
 class Solution:
     def search(self, reader: 'ArrayReader', target: int) -> int:
-        l, r = 0, get_boundary(reader) - 1
+        l, r = 0, 10000 - 1
 
         while l <= r:
             m = (l + r) // 2
             v = reader.get(m)
 
-            if v == target:
+            if v == 2147483647:
+                r = m - 1
+            elif v == target:
                 return m
             elif v > target:
                 r = m - 1
