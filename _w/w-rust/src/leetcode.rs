c1c9c65c25fc7b66f37db79dc74dc63580091efb
Che-Hsun Liu <chehsunliu@gmail.com>
2024-06-25 15:29:12 +0800

    [LeetCode] 700. Search in a Binary Search Tree

--- _w/w-rust/src/leetcode.rs
+++ _w/w-rust/src/leetcode.rs
@@ -1,38 +1,29 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
 impl Solution {
-    pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        let mut nodes = vec![];
-        let mut ptr = head;
+    pub fn search_bst(root: Option<Rc<RefCell<TreeNode>>>, val: i32) -> Option<Rc<RefCell<TreeNode>>> {
 
-        while let Some(mut v) = ptr {
-            ptr = v.next.take();
-            nodes.push(Some(v));
-        }
-
-        if nodes.is_empty() {
-            return ptr;
-        }
-
-        for i in 0..(nodes.len() - 1) {
-            let v = nodes[i].take();
-            nodes[i + 1].as_mut().map(|x| x.next = v);
-        }
-
-        nodes.last_mut().unwrap().take()
     }
 }
 
 struct Solution;
 
 // Definition for singly-linked list.
-#[derive(PartialEq, Eq, Clone, Debug)]
-pub struct ListNode {
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
     pub val: i32,
-    pub next: Option<Box<ListNode>>,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
 }
 
-impl ListNode {
+impl TreeNode {
     #[inline]
-    fn new(val: i32) -> Self {
-        ListNode { next: None, val }
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
     }
 }
