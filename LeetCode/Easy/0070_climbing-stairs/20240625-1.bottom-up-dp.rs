0872da734b431476893d87a81e19dcc7f498bb79
Che-Hsun Liu <chehsunliu@gmail.com>
2024-06-25 17:16:17 +0800

    [LeetCode] 70. Climbing Stairs (Bottom-Up / DP)

--- LeetCode/Easy/0070_climbing-stairs/20240625-1.bottom-up-dp.rs
+++ LeetCode/Easy/0070_climbing-stairs/20240625-1.bottom-up-dp.rs
@@ -1,11 +1,11 @@
 impl Solution {
     pub fn climb_stairs(n: i32) -> i32 {
         let (mut f1, mut f0) = (1, 1);
-
+        
         for _ in 1..n {
             (f1, f0) = (f1 + f0, f1);
         }
-
+        
         f1
     }
 }
