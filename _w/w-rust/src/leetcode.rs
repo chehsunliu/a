627183d9286e2b55465e30ba81ad8ca682466ea8
Che-Hsun Liu <chehsunliu@gmail.com>
2024-07-02 02:21:06 +0800

    [LeetCode] 912. Sort and Array (AC / Counting Sort / Rust)

--- _w/w-rust/src/leetcode.rs
+++ _w/w-rust/src/leetcode.rs
@@ -1,33 +1,19 @@
-use rand::Rng;
-
 impl Solution {
-    pub fn sort_array(mut nums: Vec<i32>) -> Vec<i32> {
-        f(&mut nums);
-        nums
-    }
-}
-
-fn f(nums: &mut [i32]) {
-    if nums.len() <= 1 {
-        return;
-    }
-
-    let mut rng = rand::thread_rng();
-    let i = rng.gen_range(0..nums.len());
-    nums.swap(i, nums.len() - 1);
-
-    let mut m = 0;
-    let value = *nums.last().unwrap();
-    for i in 0..(nums.len() - 1) {
-        if nums[i] < value {
-            nums.swap(i, m);
-            m += 1;
+    pub fn sort_array(nums: Vec<i32>) -> Vec<i32> {
+        let mut counts = vec![0; 100001];
+        for v in &nums {
+            counts[(*v + 50000) as usize] += 1;
         }
-    }
-    nums.swap(m, nums.len() - 1);
 
-    f(&mut nums[..m]);
-    f(&mut nums[m + 1..]);
+        let mut ans = Vec::with_capacity(nums.len());
+        for (v, count) in counts.iter().enumerate() {
+            for _ in 0..*count {
+                ans.push(v as i32 - 50000);
+            }
+        }
+
+        ans
+    }
 }
 
 // 1 2 4 5 4 9 3
