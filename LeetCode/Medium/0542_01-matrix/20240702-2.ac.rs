aa2ee2a67d976e6e425f07fd79c06b86ef77be7d
Che-Hsun Liu <chehsunliu@gmail.com>
2024-07-02 01:09:48 +0800

    [LeetCode] 542. 01 Matrix (AC / Rust)

--- LeetCode/Medium/0542_01-matrix/20240702-2.ac.rs
+++ LeetCode/Medium/0542_01-matrix/20240702-2.ac.rs
@@ -7,22 +7,17 @@ impl Solution {
         let n = mat[0].len() as i32;
 
         let mut ans = vec![vec![i32::MAX; n as usize]; m as usize];
-        let mut zeros: Vec<(i32, i32)> = vec![];
-
-        for i in 0..m {
-            for j in 0..n {
-                if mat[i as usize][j as usize] == 0 {
-                    zeros.push((i, j));
-                    ans[i as usize][j as usize] = 0;
-                }
-            }
-        }
 
         let mut queue: VecDeque<(i32, i32, i32)> = VecDeque::new();
         let mut visited: HashSet<(i32, i32)> = HashSet::new();
 
-        for zero in zeros {
-            queue.push_back((zero.0, zero.1, 0));
+        for i in 0..m {
+            for j in 0..n {
+                if mat[i as usize][j as usize] == 0 {
+                    queue.push_back((i, j, 0));
+                    ans[i as usize][j as usize] = 0;
+                }
+            }
         }
 
         while let Some((i, j, d)) = queue.pop_front() {
