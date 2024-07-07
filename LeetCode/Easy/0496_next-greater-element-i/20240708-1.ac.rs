dfce23c1b2b2f221c32ecbbb899fc67e88b2267d
Che-Hsun Liu <chehsunliu@gmail.com>
2024-07-08 00:43:51 +0800

    [LeetCode] 496. Next Greater Element I (AC / Rust)

--- LeetCode/Easy/0496_next-greater-element-i/20240708-1.ac.rs
+++ LeetCode/Easy/0496_next-greater-element-i/20240708-1.ac.rs
@@ -1,7 +1,7 @@
 impl Solution {
     pub fn next_greater_element(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
         let mut ans = vec![];
-
+        
         for target in nums1 {
             let mut prev_value = None;
             for &current_value in nums2.iter().rev() {
@@ -9,13 +9,13 @@ impl Solution {
                     ans.push(prev_value.unwrap_or(-1));
                     break;
                 }
-
+                
                 if current_value > target {
                     prev_value = Some(current_value);
                 }
             }
         }
-
+        
         ans
     }
 }
