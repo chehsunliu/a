f89f3529832851c7fe86683db961a180c8108dea
chehsunliu <chehsunliu@gmail.com>
2025-01-21 02:07:37 +0800

    [LeetCode] 49. Group Anagrams (AC / Rust)

--- _w/w-rust/src/leetcode.rs
+++ _w/w-rust/src/leetcode.rs
@@ -1,22 +1,27 @@
 use std::collections::HashMap;
 
 impl Solution {
-    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut lookup: HashMap<i32, usize> = HashMap::new();
+    pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+        let mut lookup: HashMap<[u8; 26], Vec<&str>> = HashMap::new();
+        for s in &strs {
+            let mut counter: [u8; 26] = [0; 26];
+            s.chars()
+                .for_each(|c| counter[(c as u8 - b'a') as usize] += 1);
 
-        for (i, v) in nums.iter().enumerate() {
-            lookup.insert(*v, i);
-        }
-
-        for (i, v) in nums.iter().enumerate() {
-            if let Some(j) = lookup.get(&(target - *v)) {
-                if i != *j {
-                    return vec![i as i32, *j as i32];
-                }
+            if let Some(vs) = lookup.get_mut(&counter) {
+                vs.push(s);
+            } else {
+                lookup.insert(counter, vec![s]);
             }
         }
 
-        unreachable!()
+        let mut ans: Vec<Vec<String>> = vec![];
+
+        lookup.values().for_each(|vs| {
+            ans.push(vs.iter().map(|v| v.to_string()).collect::<Vec<String>>());
+        });
+
+        ans
     }
 }
 
