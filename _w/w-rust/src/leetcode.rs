323a56ded06db0a42e3b1ef9aa3df697e844d549
chehsunliu <chehsunliu@gmail.com>
2025-01-20 23:59:29 +0800

    [LeetCode] 1. Two Sum (AC / Rust)

--- _w/w-rust/src/leetcode.rs
+++ _w/w-rust/src/leetcode.rs
@@ -1,6 +1,22 @@
+use std::collections::HashMap;
+
 impl Solution {
-    pub fn length_of_last_word(s: String) -> i32 {
-        s.split_whitespace().last().unwrap().len() as i32
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut lookup: HashMap<i32, usize> = HashMap::new();
+
+        for (i, v) in nums.iter().enumerate() {
+            lookup.insert(*v, i);
+        }
+
+        for (i, v) in nums.iter().enumerate() {
+            if let Some(j) = lookup.get(&(target - *v)) {
+                if i != *j {
+                    return vec![i as i32, *j as i32];
+                }
+            }
+        }
+
+        unreachable!()
     }
 }
 
