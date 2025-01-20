7cd73cb9a51b4494c9b74dada716325596e60603
chehsunliu <chehsunliu@gmail.com>
2025-01-21 00:50:09 +0800

    [LeetCode] 49. Group Anagrams (AC / Python)

--- _w/w-py/src/leetcode.py
+++ _w/w-py/src/leetcode.py
@@ -1,30 +1,17 @@
-from typing import Optional
-
-
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
-def solve(node: TreeNode | None, target: float, ans: TreeNode) -> int:
-    if node is None:
-        return ans.val
-
-    diff = abs(target - node.val)
-    old_diff = abs(target - ans.val)
-    ans = node if diff < old_diff or (diff == old_diff and node.val < ans.val) else ans
-
-    if node.left and target < node.val:
-        return solve(node.left, target, ans)
-    elif node.right and target > node.val:
-        return solve(node.right, target, ans)
-    else:
-        return ans.val
+import hashlib
 
 
 class Solution:
-    def closestValue(self, root: Optional[TreeNode], target: float) -> int:
-        return solve(root, target, root)
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        lookup = {}
+
+        for s in strs:
+            v = hashlib.md5("".join(sorted(s)).encode("utf-8")).hexdigest()
+            if v not in lookup:
+                lookup[v] = []
+            lookup[v].append(s)
+
+        ans = []
+        for vs in lookup.values():
+            ans.append(vs)
+        return ans
