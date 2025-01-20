101d4c25bcd0e4554042ed3c28875910fcdc3a26
chehsunliu <chehsunliu@gmail.com>
2025-01-20 23:52:22 +0800

    [LeetCode] 58. Length of Last Word (AC / Rust)

--- _w/w-rust/src/leetcode.rs
+++ _w/w-rust/src/leetcode.rs
@@ -1,22 +1,6 @@
 impl Solution {
-    pub fn is_subsequence(s: String, t: String) -> bool {
-        f(s.as_bytes(), t.as_bytes())
-    }
-}
-
-fn f(s: &[u8], t: &[u8]) -> bool {
-    if s.is_empty() {
-        return true;
-    }
-
-    if t.is_empty() {
-        return false;
-    }
-
-    if s[0] == t[0] {
-        f(&s[1..], &t[1..])
-    } else {
-        f(s, &t[1..])
+    pub fn length_of_last_word(s: String) -> i32 {
+        s.split_whitespace().last().unwrap().len() as i32
     }
 }
 
