d0ae3f5b78793413e443d741cb17cfb72afc42f1
chehsunliu <chehsunliu@gmail.com>
2025-01-20 23:39:30 +0800

    [LeetCode] 1299. Replace Elements with Greatest Element on Right Side (AC / Rust)

--- _w/w-rust/src/leetcode.rs
+++ _w/w-rust/src/leetcode.rs
@@ -1,35 +1,18 @@
-use std::collections::{HashMap, HashSet};
-
 impl Solution {
-    pub fn is_anagram(s: String, t: String) -> bool {
-        let (c1, c2) = (Self::f(s), Self::f(t));
+    pub fn replace_elements(arr: Vec<i32>) -> Vec<i32> {
+        let mut ans = vec![];
 
-        let (k1, k2): (HashSet<char>, HashSet<char>) = (
-            HashSet::from_iter(c1.keys().map(|c| *c)),
-            HashSet::from_iter(c2.keys().map(|c| *c)),
-        );
-
-        if k1.len() != k2.len() || !k1.is_subset(&k2) {
-            return false;
+        let mut m = 0;
+        for &v in arr.iter().rev() {
+            m = m.max(v);
+            ans.push(m);
         }
 
-        for c in k1.iter() {
-            if c1.get(c).unwrap() != c2.get(c).unwrap() {
-                return false;
-            }
-        }
+        ans.pop();
+        ans.reverse();
+        ans.push(-1);
 
-        true
-    }
-
-    fn f(s: String) -> HashMap<char, i32> {
-        let mut r = HashMap::new();
-
-        for c in s.chars() {
-            r.insert(c, r.get(&c).unwrap_or(&0) + 1);
-        }
-
-        r
+        ans
     }
 }
 
