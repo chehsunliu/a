f796678c0f430d82c83742e341c02ab4833cb377
chehsunliu <chehsunliu@gmail.com>
2025-01-22 02:15:37 +0800

    [LeetCode] 347. Top K Frequent Elements (AC / Rust)

--- _w/w-rust/src/leetcode.rs
+++ _w/w-rust/src/leetcode.rs
@@ -1,27 +1,19 @@
 use std::collections::HashMap;
 
 impl Solution {
-    pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-        let mut lookup: HashMap<[u8; 26], Vec<&str>> = HashMap::new();
-        for s in &strs {
-            let mut counter: [u8; 26] = [0; 26];
-            s.chars()
-                .for_each(|c| counter[(c as u8 - b'a') as usize] += 1);
-
-            if let Some(vs) = lookup.get_mut(&counter) {
-                vs.push(s);
-            } else {
-                lookup.insert(counter, vec![s]);
-            }
+    pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let mut counter: HashMap<i32, i32> = HashMap::new();
+        for v in &nums {
+            counter.insert(*v, counter.get(v).unwrap_or(&0) + 1);
         }
 
-        let mut ans: Vec<Vec<String>> = vec![];
+        let mut counter = counter
+            .iter()
+            .map(|(k, v)| (*k, *v))
+            .collect::<Vec<(i32, i32)>>();
+        counter.sort_by(|&c1, &c2| c1.1.cmp(&c2.1).reverse());
 
-        lookup.values().for_each(|vs| {
-            ans.push(vs.iter().map(|v| v.to_string()).collect::<Vec<String>>());
-        });
-
-        ans
+        counter[..k as usize].iter().map(|(k, _)| *k).collect()
     }
 }
 
